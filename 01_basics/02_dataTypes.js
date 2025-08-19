7ebc344bbfcfa65b2a2b785a96f1235a1a2692b4
Camelia Pramanick <cameliapramanick11@gmail.com>
2025-08-19 13:53:38 +0000

    container config4 done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Camelia")
+
+
+let name = "Camelia"
+let age = 19
+let isLoggedIn = false
+let state; // undefined
+let state2 = null // null
+
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "Camelia");
+console.log(typeof null); // output: object
+console.log(typeof undefined); // output: undefined
